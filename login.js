bea50ffdec8fcddb448c14f4f5f02d1329a4d4e5
Satarana <22317@edu.rybners.dk>
2025-03-25 13:27:16 +0100

    first commit

---
+++ login.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+    function validate(){
+        const usernameArray = [];
+        const passwordArray = [];
+    
+    
+        // document.getElementById("rank1").innerHTML = "None";
+        var rang = document.getElementById("user").value;
+        
+        // document.getElementById("rank1").innerHTML = rang;
+    
+        usernameArray.push(rang);
+    
+    
+    
+    
+    
+    // function ranking(){
+        let tes = "";
+        const rankArray = [];
+        rankArray.push(rang);
+    
+        rankArray.forEach(rankins);
+    
+        document.getElementById("rank1").innerHTML += tes;
+    
+        function rankins(item, index){
+             tes += index  + ": " + item + "<br>";
+        }
+    
+    
+    
+    
+        document.getElementById("")
+    
+    
+    }
+
+
