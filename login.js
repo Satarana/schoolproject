59f2ee90575ad46a57923bace081a699e8a5035e
Satarana <22317@edu.rybners.dk>
2025-03-25 14:30:08 +0100

    rwr

--- login.js
+++ login.js
@@ -3,31 +3,13 @@
 
 
     
-    document.addEventListener('DOMContentLoaded', () => {
-        const savedUsername = localStorage.getItem('username');
-        const message = document.getElementById('message');
-
-        if(savedUsername) {
-            message.textContent = `Welcome back, ${savedUsername}!`;
-        }
-
-        const form = document.getElementById('usernameForm');
-        form.addEventListener('submit', function(event){
-            event.preventDefault();
-
-        const usernameInput = document.getElementById('username').value;
-
-        localStorage.setItem('username', usernameInput)
-
-        message.textContent = `Username saved: ${usernameInput}`
-        });
-
-    })
+    
 
 
-    function validate(){
+        const rankArray = ["Ar", "As"];
         const usernameArray = [];
         const passwordArray = [];
+    function validate(){
     
     
         // document.getElementById("rank1").innerHTML = "None";
@@ -35,24 +17,22 @@
         
         // document.getElementById("rank1").innerHTML = rang;
     
-        usernameArray.push(rang);
-    
-    
-    
-    
-    
-    // function ranking(){
-        let tes = "";
-        const rankArray = [];
+        
+        
+        
+        
+        
+        // function ranking(){
         rankArray.push(rang);
-    
-        rankArray.forEach(rankins);
-    
-        document.getElementById("rank1").innerHTML += tes;
-    
-        function rankins(item, index){
-             tes += index  + ": " + item + "<br>";
+        let text = "";
+        
+        
+        for (let i = 0; i < rankArray.length; i++){
+            text += rankArray[i] + "<br>";
         }
+        document.getElementById("rank1").innerHTML = text;
+
+        
     
     
     
